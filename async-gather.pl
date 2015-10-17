b909e01beb4e27971720f510c8de425674027a9e
g <gregory.nisbet@gmail.com>
2015-10-17 12:44:18 -0700

    modified async-gather

--- async-gather.pl
+++ async-gather.pl
@@ -1,14 +1,20 @@
 use strict;
 use warnings FATAL => 'all';
 
+# generate command to get average 
+# round trip length to hostname.
+# we know in advance that each command only
+# emits one line
 sub command {
     my $host = shift;
-    qq(ping -w 1 -c 2 '$host' |) . 
+    qq(ping -w 1 -c 5 '$host' |) . 
 	q(tail -n 1 |) .
 	q(sed 's/^.*= //;s/ ms//' |) .
 	q(awk -F '/' 'END {print $2}');
 }
 
+# launch each ping in its own process and create
+# a filehandle that reads from it
 sub filehandles {
     my @handles;
     for my $command (@_) {
@@ -19,8 +25,13 @@ sub filehandles {
     @handles;
 }
 
-
-
 my @commands = map command($_), qw[google.com cnn.com wikipedia.org];
 my @fhs = filehandles(@commands);
+
+# read line from each of the ping processes sequentially
+for my $fh (@fhs) {
+    my $line = scalar <$fh>;
+    chomp $line;
+    printf "%s\n", $line || '+INF';
+}
 
